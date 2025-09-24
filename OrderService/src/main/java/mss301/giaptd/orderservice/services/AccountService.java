27abe8c35601a94a1c666c95f093e40865adc802
giap.thaidinh <giaptdhe186094@fpt.edu.vn>
2025-09-24 13:25:38 +0700

    Eureka

--- OrderService/src/main/java/mss301/giaptd/orderservice/services/AccountService.java
+++ OrderService/src/main/java/mss301/giaptd/orderservice/services/AccountService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "account-service", url = "http://localhost:8081")
+@FeignClient(name = "account-service")
 public interface AccountService {
     @GetMapping("/api/accounts/{id}")
     AccountDTO getAccount(@PathVariable("id") Long id);
--- OrderService/src/main/java/mss301/giaptd/orderservice/services/OrchidService.java
+++ OrderService/src/main/java/mss301/giaptd/orderservice/services/OrchidService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "orchid-service", url = "http://localhost:8082")
+@FeignClient(name = "orchid-service")
 public interface OrchidService {
     @GetMapping("/api/orchids/{id}")
     OrchidDTO getOrchid(@PathVariable("id") Long id);
